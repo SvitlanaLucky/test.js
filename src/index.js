5a4806d45ceae73f26257bb2d584a15867ebd4c7
SvitlanaLucky <svitlanalucky04@gmail.com>
2023-02-01 23:55:09 +0200

    modul 1.1

---
+++ src/index.js
@@ -0,0 +1,85 @@
+// const str1 = '12.49';
+// const str2 = '0.55';
+
+// console.log(str1 + str2);
+
+// const num2 = parseFloat(str2);
+// const num1 = parseFloat(str1);
+
+// console.log(Math.round(str1));
+
+//------------------------- examples ------------------------------------
+
+// 1
+
+// const apples = 100;
+// const grapes = 25;
+
+// const total = apples + grapes;
+// console.log(total);
+
+// const diff = apples - grapes;
+// console.log(diff);
+
+// 2
+
+// let students = 75;
+// students += 50;
+// console.log(students);
+
+// 3
+
+// const value = 27.4;
+
+// console.log(Math.floor(value));
+// console.log(Math.ceil(value));
+// console.log(Math.round(value));
+
+// console.log(Math.round(Math.random() * 100) + 1);
+
+// 4
+
+// const boyName = 'Alex Kych';
+// const friendName = 'Gmix';
+// const message = `${boyName} is a best friend ${friendName}`;
+
+// console.log(message);
+
+// 5
+
+// let weight = '102,3';
+// let height = '6.2';
+
+// weight = weight.replace(',', '.');
+// height = height.replace(',', '.');
+
+// const bmi = (weight / Math.pow(height, 2)).toFixed(2);
+
+// console.log(bmi);
+
+// 6
+
+const incomingValue = prompt('enter');
+const defaultValue = 10;
+
+const value = incomingValue || defaultValue;
+
+console.log(value);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
