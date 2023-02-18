8a5f5590514f6e302fd537ce3d810cb6dc12db58
SvitlanaLucky <svitlanalucky04@gmail.com>
2023-02-18 22:23:34 +0200

    add course

---
+++ src/js/course.js
@@ -0,0 +1,23 @@
+// console.log('Hello, Svitlana');
+
+const ladyS = {
+    age: 36,
+    weight: 52,
+};
+
+const ladyPs = ladyS;
+
+ladyPs.age = 26;
+
+ladyPs.height = 174;
+
+
+
+
+
+
+console.table(ladyS);
+console.table(ladyPs);
+
+
+
