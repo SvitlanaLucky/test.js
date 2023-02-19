177d9ee806c19cb5a17c0168d62f6565d7007b05
SvitlanaLucky <svitlanalucky04@gmail.com>
2023-02-19 18:12:40 +0200

    course JSON

--- src/js/course.js
+++ src/js/course.js
@@ -1,23 +1,123 @@
 // console.log('Hello, Svitlana');
 
-const ladyS = {
-    age: 36,
-    weight: 52,
-};
+// const ladyS = {
+//     name: 'Svitlana',
+//     age: 36,
+//     weight: 52,
+// };
 
-const ladyPs = ladyS;
+// const ladyPs = ladyS;
 
-ladyPs.age = 26;
+// ladyPs.age = 26;
 
-ladyPs.height = 174;
+// ladyPs.height = 174;
+// ladyS.Brokk = 3;
+
+// console.log(ladyS);
+// console.log(ladyPs);
 
 
 
+// типізація статична і динамічна -> js динамічна мова, викор. оголошення зміної const щоб не було помилок --------
+
+// let b = 9;
+
+// b = 'fdc';
+// console.log(b);
+
+// function create() {
+//     console.log('Hello everybody');
+// };
+
+// create();
+
+// create = 'Hi';
+
+// create();
+// console.log(create);
+
+// const create = () => {
+//     console.log('Hello everybody');
+// }
+
+// create();
+
+// create = 'Hi';
+
+// create();
 
 
+// ------------------------ Обєкт --------------------------------
 
-console.table(ladyS);
-console.table(ladyPs);
+// const myAlex = {
+//     name: 'Alex',
+//     age: 28,
+//     height: 176,
+//     love: true,
+// };
+// console.log(myAlex);
 
+// myAlex.weight = 74;
+// myAlex.age = 29;
+
+// console.log(myAlex);
+
+// delete myAlex.weight;
+
+// console.log(myAlex);
+
+
+// const myAlex = {
+//     name: 'Alex',
+// };
+
+// console.log(myAlex);
+
+
+// const svitlanaAlex = 'age';
+
+// myAlex[svitlanaAlex] = 7;
+
+// console.log(myAlex);
+
+
+// const name = 'Alex';
+// const age = 28;
+
+// const userProfile = {
+//     name,
+//     age,
+//     love: true,
+// };
+
+// console.log(userProfile);
+
+
+// --------------- методи --------------------------------
+
+// const myCity = {
+//     city: 'Kyiv',
+//     cityGreeting() {
+//         console.log('Hello');
+//     },
+// };
+
+// myCity.cityGreeting();
+
+
+// --------------------- JSON -------------------
+
+// const post = {
+//     title: 'My post',
+//     likesQty: 5,
+// };
+
+// console.log(post);
+
+// console.log(JSON.stringify(post));
+
+// const postStringified = JSON.stringify(post);
+
+// console.log(JSON.parse(postStringified));
 
 
