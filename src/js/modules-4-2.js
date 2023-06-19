160a8dadb1a7eb516806dd8beca7a210a92bdb33
SvitlanaLucky <svitlanalucky04@gmail.com>
2023-06-19 20:19:49 +0300

    wind 4-2

---
+++ src/js/modules-4-2.js
@@ -0,0 +1,54 @@
+// const arr = [1, 2, 5, 3, 9, 6];
+// const num = 8;
+
+// arr.forEach(function (item, idx, arr) {
+//   arr[idx] = item * num;
+// });
+
+// console.log(arr);
+// _______________________________________________________________________
+// const arr = [
+//   { name: 'Sv', age: 36 },
+//   { name: 'Alex', age: 29 },
+//   { name: 'Brokk', age: 4 },
+// ];
+
+// const result = arr.every(
+//   (user) => user.age >= 18
+// );
+// console.log(result);
+// _______________________________________________________________________________
+
+// const numbers = [
+//   2, 78, 43, 56, 77, 11, 31, 12,
+// ];
+
+// const result = [...numbers].sort(
+//   (a, b) => a - b
+// );
+
+// console.log('sort:', result);
+// console.log('numbers:', numbers);
+// __________________________________________________________________________
+
+// reduce
+
+// const str = [
+//   's',
+//   'a',
+//   'f',
+//   'B',
+//   'A',
+//   'S',
+// ];
+// const result = str.reduce(
+//   (acc, item, idx, arr) => {
+//     console.log('acc:', acc);
+//     console.log('item:', item);
+//     console.log(
+//       '_____________________________________'
+//     );
+//     return acc + item;
+//   }
+// );
+// ________________________________________________________________________
