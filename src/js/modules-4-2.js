60b18064457fc91856c30fdeda2237a9b4194810
SvitlanaLucky <svitlanalucky04@gmail.com>
2023-06-21 20:24:18 +0300

    wind 4-2

--- src/js/modules-4-2.js
+++ src/js/modules-4-2.js
@@ -52,3 +52,116 @@
 //   }
 // );
 // ________________________________________________________________________
+
+const cars = [
+  {
+    make: 'Ford',
+    model: 'Explorer',
+    type: 'suv',
+    amount: 6,
+    price: 3160,
+    onSale: false,
+  },
+  {
+    make: 'Jeep',
+    model: 'Renegade',
+    type: 'suv',
+    amount: 9,
+    price: 42500,
+    onSale: false,
+  },
+  {
+    make: 'Saab',
+    model: '9-3',
+    type: 'sedan',
+    amount: 8,
+    price: 39700,
+    onSale: true,
+  },
+];
+
+// const getCarsWithType = (cars, type) =>
+//   cars.filter(
+//     ({ type: carType }) =>
+//       carType === type
+//   );
+
+// console.table(
+//   getCarsWithType(cars, 'suv')
+// );
+// console.table(
+//   getCarsWithType(cars, 'sedan')
+// );
+
+// const getCarByModel = (cars, model) =>
+//   cars.find(
+//     ({ model: carModel }) =>
+//       model === carModel
+//   );
+
+// console.log(
+//   getCarByModel(cars, 'Renegade')
+// );
+// console.log(getCarByModel(cars, '9-3'));
+
+// const sortByAscendingAmount = (cars) =>
+//   [...cars].sort(
+//     (a, b) => a.price - b.price
+//   );
+
+// console.table(
+//   sortByAscendingAmount(cars)
+// );
+// console.table(cars);
+
+// const sortByModel = (cars, order) => {
+//   if (order === 'asc') {
+//     return [...cars].sort((a, b) =>
+//       a.model.localeCompare(b.model)
+//     );
+//   } else {
+//     return [...cars].sort((a, b) =>
+//       b.model.localeCompare(a.model)
+//     );
+//   }
+// };
+
+// console.table(sortByModel(cars, 'asc'));
+// console.table(
+//   sortByModel(cars, 'desc')
+// );
+
+// const getTotalAmount = (cars) =>
+//   cars.reduce(
+//     (acc, { amount }) => acc + amount,
+//     0
+//   );
+
+// console.log(getTotalAmount(cars));
+
+// const getModelsOnSale = (cars) =>
+//   cars
+//     .filter(({ onSale }) => onSale)
+//     .map(
+//       ({ make, model }) =>
+//         `${make}, ${model}`
+//     );
+
+// console.log(getModelsOnSale(cars));
+// ___________________________________________________________________
+
+// const str =
+//   'ghhhaijepfoghoihjraaaaahjldleuoknl';
+
+// const result = str
+//   .split('')
+//   .reduce((acc, item) => {
+//     acc.hasOwnProperty(item)
+//       ? (acc[item] += 1)
+//       : (acc[item] = 1);
+
+//     return acc;
+//   }, {});
+
+// console.log(result);
+// _________________________________________________________________________
