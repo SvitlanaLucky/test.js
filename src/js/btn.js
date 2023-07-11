7489bbcda5fd76fec98d931d6109eb1d8b2c691a
SvitlanaLucky <svitlanalucky04@gmail.com>
2023-07-11 18:49:08 +0300

    wind modules 5

--- src/js/btn.js
+++ src/js/btn.js
@@ -1,73 +1,81 @@
-// const btnAdd = document.querySelector('button[data-add]');
-// const resetBtn = document.querySelector('button[data-reset]');
-// const valueInput = document.querySelector('input[data-value]');
-// const outputEl = document.querySelector('[data-output] span');
+const btnAdd = document.querySelector(
+  'button[data-add]'
+);
+const resetBtn = document.querySelector(
+  'button[data-reset]'
+);
+const valueInput =
+  document.querySelector(
+    'input[data-value]'
+  );
+const outputEl = document.querySelector(
+  '[data-output] span'
+);
 
-// let total = 0;
+let total = 0;
 
-// btnAdd.addEventListener('click', function () {
-//     console.log('click he-he-he');
+btnAdd.addEventListener(
+  'click',
+  function () {
+    console.log('click he-he-he');
 
-//     const value = Number(valueInput.value);
+    const value = Number(
+      valueInput.value
+    );
 
-//     total += value;
-//     outputEl.textContent = total;
-//     valueInput.value = '';
+    total += value;
+    outputEl.textContent = total;
+    valueInput.value = '';
 
-//     console.log(total);
-// });
+    console.log(total);
+  }
+);
 
-// resetBtn.addEventListener('click', function () {
-//     total = 0;
-//     outputEl.textContent = total;
+resetBtn.addEventListener(
+  'click',
+  function () {
+    total = 0;
+    outputEl.textContent = total;
+  }
+);
+// ______________________________________________________________________
 
-// });
-
-
-
-
-// const global = "global";
+// const global = 'global';
 
 // if (true) {
-//   const blockA = "block A";
+//   const blockA = 'block A';
 
 //   console.log(global); // 'global'
 //   console.log(blockA); // block A
-//   console.log(blockB); // ReferenceError: blockB is not defined
-//   console.log(blockC); // ReferenceError: blockC is not defined
+//   //   console.log(blockB); // ReferenceError: blockB is not defined
+//   //   console.log(blockC); // ReferenceError: blockC is not defined
 
 //   if (true) {
-//     const blockB = "block B";
+//     const blockB = 'block B';
 
 //     console.log(global); // global
 //     console.log(blockA); // block A
 //     console.log(blockB); // block B
-//     console.log(blockC); // ReferenceError: blockC is not defined
+//     // console.log(blockC); // ReferenceError: blockC is not defined
 //   }
 // }
 
 // if (true) {
-//   const blockC = "block C";
+//   const blockC = 'block C';
 
 //   console.log(global); // global
 //   console.log(blockC); // block C
-//   console.log(blockA); // ReferenceError: blockA is not defined
-//   console.log(blockB); // ReferenceError: blockB is not defined
+//   //   console.log(blockA); // ReferenceError: blockA is not defined
+//   //   console.log(blockB); // ReferenceError: blockB is not defined
 // }
 
-
 // console.log(global); // global
-// console.log(blockA); // ReferenceError: blockA is not defined
-// console.log(blockB); // ReferenceError: blockB is not defined
-// console.log(blockC); // ReferenceError: blockC is not defined
-
+// // console.log(blockA); // ReferenceError: blockA is not defined
+// // console.log(blockB); // ReferenceError: blockB is not defined
+// // console.log(blockC); // ReferenceError: blockC is not defined
 
 // let students = 100;
 
 // students += 50;
 
 // console.log(students);
-
-
-
-
