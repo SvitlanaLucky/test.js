4068bd2505d9d2a13737c5db9e10ee3de9277a30
SvitlanaLucky <svitlanalucky04@gmail.com>
2023-07-12 19:17:12 +0300

    wind modules 5

--- src/js/modules-5-1.js
+++ src/js/modules-5-1.js
@@ -4,3 +4,51 @@
 
 // fn();
 // _________________________________________________________
+
+// 'use strict';
+// function foo() {
+//   console.log(this);
+// }
+// foo();
+
+// const boo = function () {
+//   console.log(this);
+// };
+// boo();
+
+// const arrow = () => {
+//   console.log(this);
+// };
+// arrow();
+// _________________________________________________________________________________
+
+// const objA = {
+//   name: 'User name',
+//   nickName() {
+//     console.log(this);
+//   },
+// };
+
+// objA.nickName();
+
+// const objB = {
+//   name: 'User name',
+//   nickName: () => {
+//     console.log(this);
+//   },
+// };
+
+// objB.nickName();
+
+// const objC = {
+//   name: 'User name',
+//   skills: {
+//     mySkill: 'html',
+//     nickName() {
+//       console.log(this);
+//     },
+//   },
+// };
+
+// objC.skills.nickName();
+// _______________________________________________________________________________
