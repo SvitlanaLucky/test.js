e65ce9e68ea6eccad0bab44a7978b06808468c6d
SvitlanaLucky <svitlanalucky04@gmail.com>
2023-07-14 19:47:53 +0300

    wind modules 5

--- src/js/modules-5-2.js
+++ src/js/modules-5-2.js
@@ -90,3 +90,154 @@
 // ❤️ wind + V
 console.log('❤️');
 // ___________________________________________________________________________
+
+// const user = {
+//   name: 'test user A',
+//   age: 18,
+//   sayHello() {
+//     console.log(this.name);
+//   },
+//   showAge() {
+//     console.log(`My age ${this.age}`);
+//   },
+// };
+
+// const user1 = Object.create(user);
+// user1.name = 'test user 1';
+// user1.sayHello();
+// console.log('user1:', user1);
+
+// const user2 = Object.create(user);
+// user2.showAge();
+// console.log('user2:', user2);
+// ____________________________________________________________________________________
+
+// const user = {
+//   name: 'test user B',
+//   sayHello() {
+//     console.log(this.name);
+//   },
+// };
+
+// const admin = Object.create(user);
+// admin.age = 19;
+// admin.showAge = function () {
+//   console.log(this.age);
+// };
+
+// const practiceOwner =
+//   Object.create(admin);
+// practiceOwner.name = 'Owner';
+
+// console.log(
+//   'practiceOwner:',
+//   practiceOwner
+// );
+// console.log('admin:', admin);
+
+// practiceOwner.showAge();
+// practiceOwner.sayHello();
+// ______________________________________________________________________________
+
+// class User {
+//   static counter = 0;
+//   static addUser() {
+//     this.counter += 1;
+//     console.log(
+//       `Number of users ${this.counter}`
+//     );
+//   }
+//   #location;
+//   constructor({
+//     name,
+//     email,
+//     age = 18,
+//     location = 'World',
+//   } = {}) {
+//     this.name = name;
+//     this.email = email;
+//     this.age = age;
+//     this.#location = location;
+//     User.addUser();
+//   }
+//   satHello() {
+//     // console.log(this.name);
+//   }
+
+//   #getEmail() {
+//     console.log(this.email);
+//   }
+
+//   get locale() {
+//     return this.#location;
+//   }
+//   set locale(city) {
+//     this.#location = city;
+//     this.#getEmail();
+//   }
+// }
+
+// const test = new User({
+//   name: 'User A',
+//   email: 'A@gmail.com',
+//   location: 'Lviv',
+// });
+// test.satHello();
+
+// // console.log('get:', test.locale);
+// // console.log(
+// //   'set:',
+// //   (test.locale = 'Dnipro')
+// // );
+
+// const test1 = new User({
+//   name: 'User B',
+//   email: 'B@gmail.com',
+//   age: 99,
+// });
+
+// // console.log('test:', test);
+// // console.log('test1:', test1);
+// ____________________________________________________________________________________
+
+class User {
+  constructor({
+    name,
+    age,
+    location,
+    email,
+  } = {}) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
+    this.email = email;
+  }
+}
+
+class Avatar extends User {
+  constructor({
+    name,
+    age,
+    location,
+    email,
+    damage,
+  }) {
+    super({
+      name,
+      age,
+      location,
+      email,
+    });
+    this.damage = damage;
+  }
+}
+
+const test = new Avatar({
+  name: 'A',
+  age: 100,
+  location: 'Hata',
+  email: 'Avatar@gmail.com',
+  damage: 450,
+});
+
+console.log(test);
