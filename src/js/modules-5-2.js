668ffa62e69a8e8c1a333983394936085db1ca44
SvitlanaLucky <svitlanalucky04@gmail.com>
2023-08-14 17:59:12 +0300

    wind 5

--- src/js/modules-5-2.js
+++ src/js/modules-5-2.js
@@ -512,3 +512,186 @@ console.log('❤️');
 // const sweatColor = makeChangeColor();
 // sweatColor('blue');
 // _______________________________________________________________________________
+// 1/20
+
+// const pizzaPalace = {
+//   pizzas: [
+//     'Supercheese',
+//     'Smoked',
+//     'Four meats',
+//   ],
+
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable =
+//       this.checkPizza(pizzaName);
+
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named «${pizzaName}»`;
+//     }
+
+//     return `Order accepted, preparing «${pizzaName}» pizza`;
+//   },
+// };
+// _________________________________________________________________________________
+// 2/20
+
+// const customer = {
+//   username: 'Mango',
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ['Burger', 'Pizza', 'Salad'],
+
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getDiscount() {
+//     return this.discount;
+//   },
+//   setDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -=
+//       cost - cost * this.discount;
+//     this.orders.push(order);
+//   },
+// };
+
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, 'Steak');
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+// _______________________________________________________________________________
+// 3/20
+// const historyService = {
+//   orders: [
+//     {
+//       email: 'jacob@hotmail.com',
+//       dish: 'Burrito',
+//     },
+//     {
+//       email: 'solomon@topmail.net',
+//       dish: 'Burger',
+//     },
+//     {
+//       email: 'artemis@coldmail.net',
+//       dish: 'Pizza',
+//     },
+//     {
+//       email: 'solomon@topmail.net',
+//       dish: 'Apple pie',
+//     },
+//     {
+//       email: 'jacob@hotmail.com',
+//       dish: 'Taco',
+//     },
+//   ],
+
+//   getOrdersLog() {
+//     return this.orders
+//       .map(
+//         (order) =>
+//           `email: ${order.email} dish: ${order.dish}`
+//       )
+//       .join(' - ');
+//   },
+//   getEmails() {
+//     const emails = this.orders.map(
+//       (order) => order.email
+//     );
+//     const uniqueEmails = new Set(
+//       emails
+//     );
+//     return [...uniqueEmails];
+//   },
+//   getOrdersByEmail(email) {
+//     return this.orders.filter(
+//       (order) =>
+//         order.email === email
+//     );
+//   },
+// };
+// ________________________________________________________________________________
+// 4/20
+
+// const parent = {
+//   name: 'Stacey',
+//   surname: 'Moore',
+//   age: 54,
+//   heritage: 'Irish',
+// };
+
+// const child = Object.create(parent);
+
+// child.name = 'Jason';
+// child.age = 27;
+
+// console.log(child.age);
+// ___________________________________________________________________________________
+// 7/20
+// class Car {
+//   constructor(brand, model, price) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// }
+// ____________________________________________________________________________________
+// 9/20
+
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+
+//   getPrice() {
+//     return this.price;
+//   }
+
+//   changePrice(newPrice) {
+//     this.price = newPrice;
+//   }
+// }
+// _____________________________________________________________________________________
+// 10/20
+
+// class Storage {
+//   constructor(items) {
+//     this.items = items;
+//   }
+
+//   getItems() {
+//     return this.items;
+//   }
+
+//   addItem(newItem) {
+//     this.items.push(newItem);
+//   }
+
+//   removeItem(itemToRemove) {
+//     this.items = this.items.filter(
+//       (item) => item !== itemToRemove
+//     );
+//   }
+// }
+
+// const storage = new Storage([
+//   'Nanitoids',
+//   'Prolonger',
+//   'Antigravitator',
+// ]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem('Droid');
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem('Prolonger');
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+// ________________________________________________________________________________________
